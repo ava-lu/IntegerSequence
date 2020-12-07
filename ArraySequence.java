c2095e0f87e318e4c7ea9498c5f9a331f909d04d
ava-lu <alu21@stuy.edu>
2020-12-07 13:38:30 -0500

    first commit

---
+++ ArraySequence.java
@@ -0,0 +1,11 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence {
+  // int currentIndex;
+  // int[] data;
+  //
+  // public ArraySequence(int[] other) {
+  //   for (int i=0; i<other.length; i++) {
+  //     this.data[i] = other[i];
+  //   }
+  // }
+}
