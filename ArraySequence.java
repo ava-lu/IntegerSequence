ef0672b731030a54bf38339004b5f0c214acadde
ava-lu <alu21@stuy.edu>
2020-12-07 14:18:54 -0500

    first constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,11 +1,30 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence {
-  // int currentIndex;
-  // int[] data;
-  //
-  // public ArraySequence(int[] other) {
-  //   for (int i=0; i<other.length; i++) {
-  //     this.data[i] = other[i];
-  //   }
-  // }
+  int currentIndex;
+  int[] data;
+
+  public ArraySequence(int[] other) {
+    data = new int[other.length];
+    for (int i=0; i<other.length; i++) {
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
+  public void reset() {
+    currentIndex = 0;
+  }
+  public int length() {
+    return (data.length);
+  }
+  public boolean hasNext() {
+    if (currentIndex>=data.length)return false;
+    else return true;
+  }
+  public int next() {
+    if (hasNext()==false) {
+      throw new NoSuchElementException();
+    }
+    currentIndex++;
+    return data[currentIndex-1];
+  }
 }
